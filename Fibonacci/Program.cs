bc162ce6f6405248d7148beeeb06dbb7257dedfe
Павел Бернацкий <pavlbern@gmail.com>
2020-12-02 17:20:27 +0200

    add 1, 2, 3, 5

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Friend_Pairs
+{
+    class Friend_Pairs
+    {
+        public static int friends_Pairs(int number)
+        {
+            if (number == 0 || number == 1)
+                return 1;
+            else
+                return friends_Pairs(number - 1) + (number - 1) * friends_Pairs(number - 2);
+        }
+
+        public static void Main()
+        {
+            int n = 3;
+            Console.WriteLine(friends_Pairs(n));
+        }
+    }
+}
